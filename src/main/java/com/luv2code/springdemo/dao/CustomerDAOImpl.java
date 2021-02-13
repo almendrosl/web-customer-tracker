2c1ae1c1a5a27c044d349b5397d8c982bf6b603b
Lucas Almendros <almendroslm@gmail.com>
2021-02-13 19:04:22 -0300

    it add service layer

--- src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -18,7 +18,6 @@ public class CustomerDAOImpl implements CustomerDAO {
 
 
     @Override
-    @Transactional
     public List<Customer> getCustomers() {
 
         Session currentSession = sessionFactory.getCurrentSession();
